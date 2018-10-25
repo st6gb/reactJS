09561b4ff69f4e3995a0af6c5924d689533fe6ad
Pavel Buldakov <pavel_buldakov@epam.com>
2018-10-25 12:20:55 +0400

    '5 task'

---
+++ __test__/reducers.test.js
@@ -0,0 +1,23 @@
+import active from '../src/reducers/active';
+import request from '../src/reducers/request';
+import sorting from '../src/reducers/sorting';
+
+describe('reducers',()=>{
+  it('active', ()=>{
+    const state = 'ganres';
+    const action = {type: "SET_ACTIVE", active: "title"};
+    expect(active(state, action)).toEqual("title");
+    expect(active(state, {})).toEqual("ganres");
+  })
+  it('sorting', ()=>{
+    const state = 'date';
+    const action = {type: "SORT_FILM", sortBy: "release"}
+    expect(sorting(state, action)).toEqual("release");
+    expect(sorting(state, {})).toEqual("date");
+  })
+  it('request',()=>{
+    const state = [{}];
+    const action = {type: "GET_FILM_REQUEST_SUCCESS", payload: {bla: 'bla'} };
+    expect(request(state, action)).toEqual([{bla: 'bla'}])
+  })
+})
--- src/reducers/request.js
+++ src/reducers/request.js
@@ -1,6 +1,7 @@
 const initialState = [];
 export default function request(state = initialState, action){
   if(action.type === "GET_FILM_REQUEST_SUCCESS"){
+    console.log([ ...action.payload])
     return [ ... action.payload ]
   } else if(action.type === "GET_FILM_REQUEST_ERORR"){
     return []
