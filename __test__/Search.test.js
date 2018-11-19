0bf03b28eafffa46849dd17eee4cd2f93f7ad57b
Pavel Buldakov <pavel_buldakov@epruizhw0321.moscow.epam.com>
2018-11-19 11:04:24 +0400

    tests

--- __test__/Search.test.js
+++ __test__/Search.test.js
@@ -5,6 +5,7 @@ import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import setActiveTab from '../src/store/actions/activeTab';
+import active from '../src/store/reducers/active';
 
 describe('component', () => {
   it("search",()=>{
@@ -24,14 +25,16 @@ describe('component', () => {
     expect(wrapper.find(Search).prop('activeTab')).toEqual(initialState.active)
   })
   it('check dispatch ', ()=>{
-    const initialState = {active: "genres"}
-    const mockStore = configureStore();
+    const initialState = {active: "blet"}
+    const mockStore = configureStore(active);
     let store, wrapper;
     store = mockStore(initialState);
     wrapper = mount( <Provider store={store}><BrowserRouter><ConnectedSearch /></BrowserRouter></Provider> );
     store.dispatch(setActiveTab("lala"));
+    console.log(store.getState())
     let action;
-    action = store.getActions()
-    expect(action[0].type).toBe("SET_ACTIVE");
+    action = store.getActions();
+    console.log(action[0])
+    expect(action[0].active).toBe('lala');
   })
 });
